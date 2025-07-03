62ed3f1db5e6419b69dea6b26a6c847d9bf62995
Ziv Ben-Zion <46454421+zivbz1@users.noreply.github.com>
2025-07-03 12:36:02 +0300

    Update file paths and enhance error handling in fMRIprep scripts

--- fMRIprep.bat
+++ fMRIprep.bat
@@ -2,6 +2,6 @@
 
 
 REM Run the Python script
-streamlit run "C:\Users\PsyLab-6028\Desktop\fMRIprepStreamlit\file_riunner.py"
+streamlit run "C:\Users\PsyLab-6028\Documents\GitHub\fMRIprepStreamlitPub\file_riunner.py"
 
 pause
